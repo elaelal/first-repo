e99cffed11e92f4e83fef2d405ebcd2930eaadae
elal <bliss0523@gmail.com>
2022-05-06 14:05:01 +0900

    Algorithm yeardream done

--- hello.py
+++ hello.py
@@ -8,8 +8,10 @@
 #git status
 #git andd hello.py
 #git commit
-for i in range(1,10+1):
-    if i%3==0:
+for i in range(1,20+1):
+    if i%15==0:
+        print('yeardream')
+    elif i%3==0:
         print('year')
     elif i%5==0:
         print('dream')
