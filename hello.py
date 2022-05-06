f993793d0662808d54d25adef2a6e7bbd8fe4aad
elal <bliss0523@gmail.com>
2022-05-06 13:53:15 +0900

    Test Python configuration
    print hello

---
+++ hello.py
@@ -0,0 +1,20 @@
+#과제: 1부터 20까지 반복하는 과정에서 3의배수일 경우, year를 출력하라
+#추가 과제: 5의 배수일때, dream출력
+#나머지 모든 경우는 숫자 그대로 출력
+
+
+#vi hello.py
+#python (or python3) hello.py
+#git status
+#git andd hello.py
+#git commit
+for i in range(1,10+1):
+    if i%3==0:
+        print('year')
+    else:
+        print(i)
+
+
+
+        
+
